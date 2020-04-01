d0e7e88d60482d9c459ceebdb2f746799b9b7bbf
narenraj701 <60877115+narenraj701@users.noreply.github.com>
2020-04-01 17:44:36 +0530

    creates object only if it does not exist-singleTon

--- Java Assignments/Assignment5/naren/assignment/singleton/singleTon.java
+++ Java Assignments/Assignment5/naren/assignment/singleton/singleTon.java
@@ -2,7 +2,11 @@ package naren.assignment.singleton;
 
 public class singleTon {
     private String s;
-    public static singleTon initialize(String val){
+    public static singleTon initialize(String val,singleTon obj2){
+        if((obj2 instanceof singleTon)){
+            obj2.s=val;
+            return obj2;
+        }
         singleTon singleton=new singleTon();
         singleton.s=val;
         return singleton;
